ad1e10ab8216438eec7d7441de4d7580a4340fa4
Rob A <robarnold100@hotmail.com>
2022-04-28 09:45:35 -0300

    wallet balance

--- index.js
+++ index.js
@@ -3,13 +3,26 @@ const {
     PublicKey,
     custerApiUrl,
     Keypair,
-    LAMPORTS_PER_SOL
+    LAMPORTS_PER_SOL, clusterApiUrl
 } = require("@solana/web3.js")
 
 const wallet = new Keypair()
 
-const publicKey = wallet._keypair.publicKey
+const publicKey = new PublicKey(wallet._keypair.publicKey)
 const secretKey = wallet._keypair.secretKey
 
-console.log(publicKey)
-console.log(secretKey)
+const getWalletBalance = async() => {
+    try {
+        const connection = new Connection(clusterApiUrl('devnet'), 'confirmed')
+        const walletBalance = await connection.getBalance(publicKey)
+        console.log(`wallet balance is ${walletBalance}`)
+    } catch(err) {
+        console.error(err)
+    }
+}
+
+const main = async() => {
+    await getWalletBalance()
+}
+
+main()
