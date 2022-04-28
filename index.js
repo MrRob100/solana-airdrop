1c42f5053aeab44ae7736513072d982a17c56798
Rob A <robarnold100@hotmail.com>
2022-04-28 09:32:50 -0300

    init

---
+++ index.js
@@ -0,0 +1,19 @@
+// const {
+//     Connection,
+//     PublicKey,
+//     custerApiUrl,
+//     Keypair,
+//     LAMPORTS_PER_SOL
+// } = require("@solana/web3.js")
+//
+// const wallet = new Keypair()
+//
+// const publicKey = wallet._keypair.publicKey
+// const secretKey = wallet._keypair.secretKey
+//
+// console.log(publicKey)
+// console.log(secretKey)
+
+const faf = require("@solana/web3.js");
+
+console.log(faf);
